07938ded6627583978df781d36a4dda779cdb8fa
Nathan <126289303+flash2974@users.noreply.github.com>
2024-10-13 10:12:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from Fonctions import AutoSUAPS
 
 LOGIN_URL = 'https://cas6.univ-nantes.fr/esup-cas-server/login?service=https%3A%2F%2Fu-sport.univ-nantes.fr%2Fcas%2F'
-
+username = ...
+password = ...
 
 auto = AutoSUAPS(username, password, LOGIN_URL)
 auto.reserverCreneau()
