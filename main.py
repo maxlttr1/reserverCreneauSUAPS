e81e3621a37b987760eecfe83188cbc158f57e58
Nathan <nathan.domenichini@gmail.com>
2024-10-19 10:48:05 +0200

    ajout dockeristion

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from Fonctions import AutoSUAPS
 
 LOGIN_URL = 'https://cas6.univ-nantes.fr/esup-cas-server/login?service=https%3A%2F%2Fu-sport.univ-nantes.fr%2Fcas%2F'
-username = ...
-password = ...
+username = 'E24A014X'
+password = "pH9haVAy9"
 
 auto = AutoSUAPS(username, password, LOGIN_URL)
-
-auto.reserverCreneau() # Commencer par ça
-# auto.reserverCreneau(14, 18, 21) # Réserve les créneaux
+auto.reserverCreneau(14, 18, 21)
