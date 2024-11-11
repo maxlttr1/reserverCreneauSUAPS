636dc5984bba258c0eca049188800b0f2f9e5765
Nathan <nathan.domenichini@gmail.com>
2024-11-11 12:05:18 +0100

    ajout schedule

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Fonctions import AutoSUAPS
 from dotenv import load_dotenv
 from os import getenv
 import time
+import schedule
 
 load_dotenv()
 
@@ -41,9 +42,14 @@ ids_creneaux_a_resa = ['a67c920a-fc66-452c-8d07-5d7206a44f5b',
                        '6ab242fa-79cd-435a-a71b-5dceb122b775',
                        '369278cd-f3e2-4432-b6b3-ca7437f11854']
 
-while True:
+def actions() :
     auto = AutoSUAPS(USERNAME, PASSWORD, LOGIN_URL)
     auto.reserverCreneau(ids_creneaux_a_resa)
-    time.sleep(60)
     
-# schedule.every().saturday.at("10:01").do(actions)
+schedule.every().saturday.at("12:01").do(actions)
+schedule.every().thursday.at("21:02").do(actions)
+    
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+    
