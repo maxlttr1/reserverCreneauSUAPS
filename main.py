4007418975b99d285bfcc6fc6c678805c6d45621
Nathan <nathan.domenichini@gmail.com>
2025-01-13 19:44:44 +0100

    ajt

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ Si vous lancez le programme pour la premiere fois, 2 solutions :
 - vous voulez juste essayer une fois
 - vous voulez réserver de manière récurrente
 
-Pour la premirère solution, il vous suffit d'appeler la fonction reserverCreneau sans argument,
+Pour la première solution, il vous suffit d'appeler la fonction reserverCreneau sans argument,
 la fonction vous demandera un input pour savoir ce que vous voulez réserver.
 
 Sinon, utilisez ce bout de code pour afficher les IDs des activités que vous souhaitez réserver,
@@ -31,9 +31,9 @@ auto.reserverCreneau(ids_creneaux_a_resa)
 '''
 
 
-'''# 1ère solution
-auto = AutoSUAPS(USERNAME, PASSWORD)
-auto.printIDs()'''
+# 1ère solution
+# auto = AutoSUAPS(USERNAME, PASSWORD) # Décommentez
+# auto.printIDs() # Décommentez
 
 
 # 2ème solution
@@ -46,7 +46,8 @@ def actions(creneaux = ids_creneaux_a_resa) :
     auto.reserverCreneau(creneaux)
 
 if __name__ == '__main__' :
-
+    
+    # Adapter selon jour & heure
     schedule.every().saturday.at("12:02").do(actions)
     schedule.every().thursday.at("19:32").do(actions)
 
