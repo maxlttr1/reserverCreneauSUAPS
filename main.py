b4d122e91fe1e96c1eb62905a02f8b4f48afae5e
Nathan <nathan.domenichini@gmail.com>
2025-01-21 21:23:29 +0100

    fix

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ if __name__ == '__main__' :
     auto = AutoSUAPS(USERNAME, PASSWORD)
     setAllSchedules(auto)
     counter = 0
-    old_run = datetime.datetime(1979, 1, 1)
+    old_run = datetime.datetime(1970, 1, 1)
     while datetime.datetime.now().second != 0 :
         time.sleep(1)
 
