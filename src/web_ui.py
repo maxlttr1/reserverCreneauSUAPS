da872e3e145526298909223996ab7b7bb5c67714
Maxime <maxime.lettier@protonmail.com>
2025-04-03 00:05:38 +0200

    working container

--- src/web_ui.py
+++ src/web_ui.py
@@ -68,7 +68,6 @@ def login():
             login_user(user)
             return redirect(url_for('home'))
 
-        flash('Invalid username or password!', 'danger')
     return render_template('login.html')
 
 @app.route('/logout')
