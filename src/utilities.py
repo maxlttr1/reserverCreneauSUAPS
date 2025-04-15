9e786e779835f60345aa285b230b1c5354f95ccf
Nathan <126289303+flash2974@users.noreply.github.com>
2025-04-15 22:03:00 +0200

    Update utilities.py v2

--- src/utilities.py
+++ src/utilities.py
@@ -45,6 +45,9 @@ def setAllSchedules(auto):
 
 
 def setDefaultSchedules(auto) :
+    data = {"ids_resa": ["a67c920a-fc66-452c-8d07-5d7206a44f5b", "c12b09b0-8660-4b3c-9711-983317af0441", "eba1eb76-55b8-4ae4-a067-6182f3e6707b"]}
+    with open(os.path.join(BASE_DIR, '../config/config.json'), 'w') as file :
+        json.dump(data, file, indent=4)
     schedule.every().tuesday.at("20:03", "Europe/Paris").do(actions, auto)
     schedule.every().thursday.at("19:33", "Europe/Paris").do(actions, auto)
     schedule.every().wednesday.at("21:47", "Europe/Paris").do(actions, auto)
