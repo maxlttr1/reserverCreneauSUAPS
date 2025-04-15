831083344ca3bfc040575d9c7679aaa64e0266a7
Nathan <126289303+flash2974@users.noreply.github.com>
2025-04-15 23:05:14 +0200

    clear schedule before renewing it

--- src/utilities.py
+++ src/utilities.py
@@ -40,6 +40,7 @@ def setSchedule(day, hour, auto) :
 
 
 def setAllSchedules(auto):
+    schedule.clear()
     for creneau in auto.getSchedules() :
         setSchedule(creneau['day'], creneau['hour'], auto)
 
@@ -48,6 +49,7 @@ def setDefaultSchedules(auto) :
     data = {"ids_resa": ["a67c920a-fc66-452c-8d07-5d7206a44f5b", "c12b09b0-8660-4b3c-9711-983317af0441", "eba1eb76-55b8-4ae4-a067-6182f3e6707b"]}
     with open(os.path.join(BASE_DIR, '../config/config.json'), 'w') as file :
         json.dump(data, file, indent=4)
+    schedule.clear()
     schedule.every().tuesday.at("20:03", "Europe/Paris").do(actions, auto)
     schedule.every().thursday.at("19:33", "Europe/Paris").do(actions, auto)
     schedule.every().wednesday.at("21:47", "Europe/Paris").do(actions, auto)
